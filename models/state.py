df54ad8bdef505b094436d9bcce1d1ebd0e9a262
bo1atit0 <shorungbebolatito@gmail.com>
2024-06-27 15:37:07 +0100

    storage

--- models/state.py
+++ models/state.py
@@ -4,9 +4,26 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
+    city = relationship("City", backref="state", cascade="all, delete-orphan")
+
+
+    @property
+    def cities(self):
+        """
+        Getter attribute cities that return
+        the list of city instances
+        """
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+                return city_list
